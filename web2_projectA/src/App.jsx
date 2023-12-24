0fc517193677cc6728d5dffa6ae346fb0cd26b98
au-lex <aulexc4d2@gmail.com>
2023-12-24 02:58:48 +0100

    done with product page

--- web2_projectA/src/App.jsx
+++ web2_projectA/src/App.jsx
@@ -5,6 +5,8 @@ import Task1 from './Component/Task1'
 import Task2 from './Component/Task2'
 import Task3 from './Component/Task3'
 import Task4 from './Component/Task4'
+import Featurepro from './Component/Featurepro'
+import Newarrival from './Component/Newarrival'
 
 function App() {
  
@@ -12,7 +14,9 @@ function App() {
   return (
   <>
  <Task1 />
+ <Newarrival/>
   <Task2 />
+ <Featurepro/>
   <Task3 />
   <Task4 />
   </>
--- web2_projectA/src/Component/Task4.jsx
+++ web2_projectA/src/Component/Task4.jsx
@@ -1,16 +1,80 @@
-import React from 'react'
+// Footer.js
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
-const Task4 = () => {
+const Footer = () => {
   return (
-    <div>
+    <footer className="bg-gray-800 text-white py-8">
+      <div className="container mx-auto flex flex-wrap justify-between">
+        <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+          <h2 className="text-xl  mb-2">Connect With Us</h2>
+          <p>Email: info@headphones.com</p>
+          <p>Phone: +1 (555) 123-4567</p>
+        </div>
+        <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+          <h2 className="  mb-1">Quick Links</h2>
+          <ul>
+            <li>
+              <a href="#home" className="hover:text-blue-500">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#products" className="hover:text-blue-500">
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="hover:text-blue-500">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="hover:text-blue-500">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full lg:w-1/3">
+          <h2 className=" ">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a href="#" className="text-xl hover:text-blue-500">
+              <FaFacebook />
+            </a>
+            <a href="#" className="text-xl hover:text-blue-500">
+              <FaTwitter />
+            </a>
+            <a href="#" className="text-xl hover:text-blue-500">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 text-center">
+        <div className="mb-4">
+          <h2 className="text-xl  mb-2">Subscribe to Our Newsletter</h2>
+          <p className="text-gray-400">Get updates on the latest headphones and promotions.</p>
+        </div>
+        <form className="flex items-center justify-center">
+          <input
+            type="email"
+            placeholder="Your email address"
+            className="py-2 px-4 border border-gray-500 rounded-l focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-r hover:bg-blue-600 focus:outline-none"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
+      <div className="mt-8 text-center">
+        <p>&copy; 2023 Headphones Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-
-<main>
-    <h1>task 4</h1>
-</main>
-
-    </div>
-  )
-}
-
-export default Task4
+export default Footer;
